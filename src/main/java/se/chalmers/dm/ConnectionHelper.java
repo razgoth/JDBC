7758949307e08b4722bea102b1c7e663ec66b270
Idrees <ahmadid@student.chalmers.se>
2020-02-27 18:18:56 +0100

    ConnectionHelper is Done

--- src/main/java/se/chalmers/dm/ConnectionHelper.java
+++ src/main/java/se/chalmers/dm/ConnectionHelper.java
@@ -10,9 +10,15 @@ public class ConnectionHelper {
     private static final String DB_PASSWORD = "Teach321";
     private static final String DB_URL = "jdbc:postgresql://localhost:5432/websitedb";
 
-    public static Connection createPostgresConnection() throws SQLException, ClassNotFoundException {
-        Class.forName(DRIVER_CLASS);
-        Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+    public static Connection createPostgresConnection() {
+        Connection connection = null;
+        try {
+            Class.forName(DRIVER_CLASS);
+            connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+        } catch (Exception e) {
+            System.out.println("Connection Failed: " + e.getLocalizedMessage());
+        }
+
         return connection;
     }
 }
